2f59d953034d5dbd15b7b5774b667ce3c174c275
mindingyao <1915552319@qq.com>
2021-12-23 18:29:43 +0800

    首次提交

---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,20 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    //@Param("") 取别名
+    //需要在<if>里使用而且只有一个参数必须加@Param
+    int selEctDiscussPostRows(@Param("userId") int userId);
+
+
+
+}
---
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -0,0 +1,62 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Date;
+import java.util.List;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTests {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Test
+    void testSelectUser() {
+        User user = userMapper.selectByName("liubei");
+        System.out.println(user);
+    }
+
+    @Test
+    void testInsertUser() {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("123456");
+        user.setSalt("abc");
+        user.setEmail("1915552319@qq.com");
+        user.setHeaderUrl("http://www.nowcder.com/101.png");
+        user.setCreateTime(new Date());
+
+        int rows = userMapper.insertUser(user);
+        System.out.println(rows);
+        System.out.println(user.getId());
+    }
+
+    @Test
+    public void updateUser() {
+
+    }
+
+    @Test
+    public void testSelectPosts() {
+        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(0, 0, 10);
+        for(DiscussPost post : list) {
+            System.out.println(post);
+        }
+
+        int rows = discussPostMapper.selEctDiscussPostRows(149);
+        System.out.println(rows);
+    }
+
+
+}
