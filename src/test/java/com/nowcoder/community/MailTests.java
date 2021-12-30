a04beab78041278787ba07803573a17973046ed2
mindingyao <1915552319@qq.com>
2021-12-30 19:49:45 +0800

    第一次提交

---
+++ src/test/java/com/nowcoder/community/MailTests.java
@@ -0,0 +1,36 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("1915552319@qq.com", "Test", "Welcome");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context conetxt = new Context();
+        conetxt.setVariable("username", "Sunday");
+
+        String content = templateEngine.process("/mail/demo", conetxt);
+        System.out.println(content);
+
+        mailClient.sendMail("1915552319@qq.com", "HTML", content);
+    }
+}
